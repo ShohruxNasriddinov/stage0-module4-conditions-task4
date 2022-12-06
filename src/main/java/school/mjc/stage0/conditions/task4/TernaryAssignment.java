aeb053533a70241857574647f54292cdd005bfa5
ShohruxNasriddinov <shohshoh1104@gmail.com>
2022-12-06 10:55:54 +0500

    commit task4

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
+        int maxNum = first>second?first:second;
+        System.out.println(first==maxNum?10:-10);
     }
 }
